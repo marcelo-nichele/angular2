d117ca1a1f4d223dffb94a130992eda47bad3247
Marcelo Nichele <marcelo.nichele@gmail.com>
2016-10-18 22:47:22 -0200

    adicionando rotas

--- angular-tour-of-heroes/app/hero.service.ts
+++ angular-tour-of-heroes/app/hero.service.ts
@@ -8,9 +8,13 @@ export class HeroService {
     return Promise.resolve(HEROES);
   }
   getHeroesSlowly(): Promise<Hero[]> {
-  return new Promise<Hero[]>(resolve =>
+  	return new Promise<Hero[]>(resolve =>
     setTimeout(resolve, 2000)) // delay 2 seconds
     .then(() => this.getHeroes());
-}
+  }
+  getHero(id: number): Promise<Hero> {
+  	return this.getHeroes()
+  			   .then(heroes => heroes.find(hero => hero.id === id));
+  }
 
 }
