bda5be041d792e8d591be5164cd67ea62c71d48e
Marcelo Nichele <marcelo.nichele@gmail.com>
2017-12-14 23:54:37 -0200

    Adicionado servico

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,3 +1,4 @@
+import { CursosService } from './cursos.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,14 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
   
   nomePortal: string;
-  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+  cursos: string[];
 
-  constructor() {
+  constructor(private cursosService: CursosService) {
     this.nomePortal = 'http://angular.io';
-    for (let i = 0; i < this.cursos.length; i++) {
-      let curso = this.cursos[i];
-      
-    }
+    /*for (let i = 0; i < this.cursos.length; i++) {
+      let curso = this.cursos[i];      
+    }*/
+
+    // var servico = new CursosService();
+    this.cursos = this.cursosService.getCursos();
   }
 
 
