e95ef24560b9c62b21a85f950a5bba2cce95fa58
Marcelo Soares Nichele <marcelo-nichele@PRO-PAE-7037.PROCERGS.REDERS>
2019-10-30 17:17:41 -0300

    deletando tudo

--- angular-tour-of-heroes/app/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-
-const platform = platformBrowserDynamic();
-console.log('test 4 logging...');
-platform.bootstrapModule(AppModule);
